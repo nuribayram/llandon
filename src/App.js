f9f279e4ae613e1f386e8a492324f78a8475d3ce
Nuri Bayram <nuri.bayram@me.com>
2021-01-31 12:02:51 +0100

    changed text on app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in less than a day or two
         </a>
       </header>
     </div>
